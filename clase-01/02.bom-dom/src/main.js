026e237638a2084bc9f0cec2cecf975ea58ac08b
Betastica <betaniarojas36@gmail.com>
2026-06-13 19:43:10 -0300

    Empezando con DOM y BOM

---
+++ clase-01/02.bom-dom/src/main.js
@@ -0,0 +1,17 @@
+import './style.css'
+
+console.warn('// ! DOM -> document object model')
+console.log(document)
+
+console.warn('// ! BOM -> browser object model')
+// https://developer.mozilla.org/es/docs/Web/API
+
+console.log(window)
+
+
+// ECMAScript (Genera los estandares para que JS se pueda utilizar en los navegadores y el backend de la misma manera)
+
+// https://ecma-international.org/
+
+// JavaScript no es ECMAScript -> ECMA nos da pautas para poder escribir JS correctamente
+// Que el navegador agrega (APIs) que nos ayudan en JS pero no son parte de JS -> (BOM) -> Fetch, Geolocation, HTMLColletion
