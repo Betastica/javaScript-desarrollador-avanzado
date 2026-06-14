b6afc0f878e095a35d377f18a7c58435bf242cbc
Betastica <betaniarojas36@gmail.com>
2026-06-14 12:45:32 -0300

    Avanzando con css

--- clase-02/01.bom-dom.cont/src/main.js
+++ clase-02/01.bom-dom.cont/src/main.js
@@ -1,54 +1,27 @@
 import './style.css'
 
-console.warn('// ! DOM -> document object model')
-console.log(document)
+console.warn('//! Crear elementos de DOM')
 
-console.warn('// ! BOM -> browser object model')
-// https://developer.mozilla.org/es/docs/Web/API
+const contenedorPrincipal = document.getElementById('contenedor-principal')
+console.log(contenedorPrincipal)
 
-console.log(window)
+const articulo = document.createElement('article')
+articulo.textContent = "Hola mundo"
+articulo.classList.add('w-50','h-30','bg-red-600','text-center','text-white')
+contenedorPrincipal.appendChild(articulo)
 
+// eliminar un nodo html
 
-// ECMAScript (Genera los estandares para que JS se pueda utilizar en los navegadores y el backend de la misma manera)
+articulo.remove()
 
-// https://ecma-international.org/
+// volver a agregar
 
-// JavaScript no es ECMAScript -> ECMA nos da pautas para poder escribir JS correctamente
-// Que el navegador agrega (APIs) que nos ayudan en JS pero no son parte de JS -> (BOM) -> Fetch, Geolocation, HTMLColletion
+contenedorPrincipal.appendChild(articulo)
 
-console.log(window.location.href)
+// Eventos
 
-const btnIr = document.getElementById('btn-ir')
-console.log(btnIr)
+const cuadroDeTexto = document.querySelector('input')
+console.log(cuadroDeTexto)
 
-btnIr.addEventListener('click', function(){
-    window.location.href = 'https://google.com'
-})
-
-console.warn('//! Selección de elementos en DOM')
-
-console.warn('Tradicionales')
-//* getElementById (Mejor que querySelector si trabajamos con ID)
-//* getElementsByClassName
-//* getElementsByTagName
-
-console.warn('Modernos')
-//* querySelector
-//* querySelectorAll
-
-// Utilizo todos los selectores que vengo utilizando en CSS con querySelector
-
-// Si quiero seleccionar un ID -> #
-// Si quiero seleccionar una CLASE -> .
-// Si quiero seleccionar un TAG -> (nombre tag)
-
-// const tituloPrincipal = document.querySelector('h1')
-//const tituloPrincipal = document.querySelector('.text-3xl.text-green-600')
-
-const tituloPrincipal = document.querySelector('#titulo-principal')
-
-console.log(tituloPrincipal);
-
-const subtitulo = document.querySelector('#subtitulo')
-
-subtitulo.classList.add('text-green-600', 'text-2xl')
+cuadroDeTexto.setAttribute('id', '5')
+cuadroDeTexto.classList.add('bg-green-700')
